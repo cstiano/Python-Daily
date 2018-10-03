821ea1a77c4908490866faf78162d2ebfc0b6a2f
cso <cso@cin.ufpe.br>
2018-10-03 14:50:55 -0300

    function done

--- miscellaneous/testing_hash_code.py
+++ miscellaneous/testing_hash_code.py
@@ -1,10 +1,11 @@
 
 ar = []
+ar2 = []
 for i in xrange(12):
 	for j in xrange(12):
 		ar.append(ord(str(i)[0])*len(str(i))+ord(str(j)[0])*len(str(j))+int(str(i)+str(j)))	
+		ar2.append(str(i)+'-'+str(j))
+ar2.sort()
 
-ar.sort()
-
-for i in ar:
+for i in ar2:
 	print i
