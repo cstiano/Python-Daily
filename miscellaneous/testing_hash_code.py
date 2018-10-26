18b488285458ec790f4105dc530a70d457c8e0ce
cso <cso@cin.ufpe.br>
2018-10-26 15:21:51 -0300

    changes

--- miscellaneous/testing_hash_code.py
+++ miscellaneous/testing_hash_code.py
@@ -5,6 +5,7 @@ for i in xrange(12):
 	for j in xrange(12):
 		ar.append(ord(str(i)[0])*len(str(i))+ord(str(j)[0])*len(str(j))+int(str(i)+str(j)))	
 		ar2.append(str(i)+'-'+str(j))
+
 ar2.sort()
 
 for i in ar2:
